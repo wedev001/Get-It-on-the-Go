5e177a5585fb682a425124a1c9c28275c8ca7f66
Suraj Kumar <webdev.2301@gmail.com>
2024-05-15 22:26:01 +0530

    Update App.js

--- mern_app/src/App.js
+++ mern_app/src/App.js
@@ -1,85 +1,73 @@
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { useEffect, useState } from 'react';
 import SummaryApi from './common';
 import Context from './context';
 import { useDispatch } from 'react-redux';
 import { setUserDetails } from './store/userSlice';
-import axios from 'axios';
 
-const apiUrl = 'https://get-it-on-the-go-3o1f.vercel.app/login'; // Add Vercel API link
-
-const fetchUserDetails = async () => {
-  try {
-    const response = await fetch(`${apiUrl}${SummaryApi.current_user.url}`, {
-      method: SummaryApi.current_user.method,
-      credentials: 'include',
-    });
-    const userData = await response.json();
-    return userData;
-  } catch (error) {
-    console.error('Error fetching user details:', error);
-    return null;
-  }
-};
-
-const fetchCartProductCount = async () => {
-  try {
-    const response = await fetch(`${apiUrl}${SummaryApi.addToCartProductCount.url}`, {
-      method: SummaryApi.addToCartProductCount.method,
-      credentials: 'include',
-    });
-    const cartData = await response.json();
-    return cartData.data.count;
-  } catch (error) {
-    console.error('Error fetching cart product count:', error);
-    return 0;
-  }
-};
 
 function App() {
-  const dispatch = useDispatch();
-  const [cartProductCount, setCartProductCount] = useState(0);
-  const [userDetails, setUserDetailsState] = useState(null);
+  const dispatch = useDispatch()
+  const [cartProductCount,setCartProductCount] = useState(0)
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const userData = await fetchUserDetails();
-      if (userData) {
-        dispatch(setUserDetails(userData.data));
-        setUserDetailsState(userData.data);
-      }
-    };
+  const fetchUserDetails = async()=>{
+    const dataResponse = await fetch(SummaryApi.current_user.url,{
+      method : SummaryApi.current_user.method,
+      credentials : 'include'
+    })
 
-    const fetchCartData = async () => {
-      const cartCount = await fetchCartProductCount();
-      setCartProductCount(cartCount);
-    };
+    const dataApi = await dataResponse.json()
 
-    fetchUserData();
-    fetchCartData();
-  }, []);
+    if(dataApi.success){
+      dispatch(setUserDetails(dataApi.data))
+    }
 
+    // console.log("data-user",dataResponse)
+  }
+
+  const fetchUserAddToCart = async() => {
+    const response = await fetch(SummaryApi.addToCartProductCount.url,{
+      method : SummaryApi.addToCartProductCount.method,
+      credentials : 'include'
+    })
+
+    const dataApi = await response.json()
+    // console.log("dataApi",dataApi)
+    setCartProductCount(dataApi?.data?.count)
+  }
+   
+  useEffect(()=>{
+     /*user Details*/
+     
+      fetchUserDetails();
+      fetchUserAddToCart()
+
+  },[])
   return (
-    <Context.Provider
-      value={{
-        userDetails,
+    <>
+    <Context.Provider value={{
+        fetchUserDetails, // user detail fetch 
         cartProductCount,
-        fetchUserDetails,
-        fetchCartProductCount,
-      }}
-    >
-      <ToastContainer position="top-center" />
-      <Header />
-      <main className="min-h-[calc(100vh-100px)] pt-16">
-        <Outlet />
+        fetchUserAddToCart
+       
+    }}>
+      <ToastContainer 
+        position='top-center'
+      />
+      
+      <Header/>
+      <main className='min-h-[calc(100vh-100px)] pt-16'>
+        <Outlet/>
       </main>
-      <Footer />
+      <Footer/>
     </Context.Provider>
+  </>
   );
 }
 
